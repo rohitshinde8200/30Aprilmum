77108c6dc992fa4d668ab6613a87ef764da85390
lenovo <lenovo@DESKTOP-EVFMHN1>
2022-09-11 14:25:33 +0530

    [Rohit]: created 2 new clasess demo1 & demo2

---
+++ src/sample1/demo.java
@@ -0,0 +1,15 @@
+package sample1;
+
+public class demo 
+
+{
+	public static void main(String[] args)
+	
+	{
+		System.out.println("hiii");
+		
+		
+		System.out.println("hello");
+	}
+
+}
---
+++ src/sample1/demo2.java
@@ -0,0 +1,13 @@
+package sample1;
+
+public class demo2 
+
+{
+	public static void main(String[] args) 
+	
+	{
+		System.out.println("Good Morning");
+		
+	}
+
+}
